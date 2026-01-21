b792a798e780986d28aa52509b5bdc87ab8b85b2
XiaoYiUce <xiaoyi@lemonyi.com>
2026-01-21 19:41:11 +0800

    新增积分记录查询页

---
+++ ExilumBBS/Models/Response/Point/Scorelog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ExilumBBS.Models.Response.Point
+{
+    public class Scorelog
+    {
+        /// <summary>
+        /// 使用积分数
+        /// </summary>
+        [JsonPropertyName("score")]
+        public int Score { get; set; }
+
+        /// <summary>
+        /// 积分用途
+        /// </summary>
+        [JsonPropertyName("reason")]
+        public string Reason { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 记录时间
+        /// </summary>
+        [JsonPropertyName("log_time")]
+        public string LogTime { get; set; } = string.Empty;
+    }
+}
---
+++ ExilumBBS/Models/Response/Point/ScorelogResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ExilumBBS.Models.Response.Point
+{
+    public class ScorelogResponse
+    {
+        /// <summary>
+        /// 记录列表
+        /// </summary>
+        [JsonPropertyName("list")]
+        public List<Scorelog>? List { get; set; }
+        /// <summary>
+        /// 总记录数量
+        /// </summary>
+        [JsonPropertyName("total")]
+        public int Total { get; set; }
+    }
+}
