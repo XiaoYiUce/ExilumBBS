eff0f2c69283ef4e0384fa15858c9e12695757db
XiaoYiUce <xiaoyi@lemonyi.com>
2026-02-08 20:15:43 +0800

    适配OSS上传

---
+++ ExilumBBS/Models/Response/Signature/SignatureResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ExilumBBS.Models.Response.Signature
+{
+    /// <summary>
+    /// OSS签名申请回调
+    /// </summary>
+    public class SignatureResponse
+    {
+        [JsonPropertyName("accessid")]
+        public string AccessId { get; set; } = string.Empty;
+        [JsonPropertyName("host")]
+        public string Host { get; set; } = string.Empty;
+        [JsonPropertyName("cdn_host")]
+        public string CDNHost { get; set; } = string.Empty;
+        [JsonPropertyName("expire")]
+        public long Expire { get; set; }
+        [JsonPropertyName("signature")]
+        public string Signature { get; set; } = string.Empty;
+        [JsonPropertyName("policy")]
+        public string Policy { get; set; } = string.Empty;
+        [JsonPropertyName("dir")]
+        public string Dir { get; set; } = string.Empty;
+        [JsonPropertyName("callback")]
+        public string Callback { get; set; } = string.Empty;
+    }
+}
--- ExilumBBS/Utils/HttpTools.cs
+++ ExilumBBS/Utils/HttpTools.cs
@@ -1,11 +1,13 @@
 ﻿using ExilumBBS.Models.Response;
 using ExilumBBS.Services;
 using ExilumBBS.State;
+using Flurl.Http;
 using Masa.Blazor;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
@@ -90,6 +92,13 @@ namespace ExilumBBS.Utils
             return null;
         }
 
+        /// <summary>
+        /// HTTP POST封装
+        /// </summary>
+        /// <param name="requestUrl"></param>
+        /// <param name="data"></param>
+        /// <param name="noToken"></param>
+        /// <returns></returns>
         public async Task<BBSResponse?> PostAsync(string requestUrl, object? data, bool noToken = false)
         {
             if (_httpClient.DefaultRequestHeaders.Contains("Authorization"))
@@ -142,6 +151,116 @@ namespace ExilumBBS.Utils
             }
 
             return null;
+        }
+
+        /// <summary>
+        /// 上传文件至OSS
+        /// </summary>
+        /// <param name="ossHost">OSS服务器请求地址</param>
+        /// <param name="policy">OSS Policy</param>
+        /// <param name="accessKeyId">OSS AccessKeyId</param>
+        /// <param name="signature">OSS Signature</param>
+        /// <param name="file">从系统内选的文件</param>
+        /// <returns>上传后文件名</returns>
+        public async Task<string?> UploadToOSS(string ossHost, string policy, string accessKeyId, string dir, string signature, FileResult file)
+        {
+            try
+            {
+                using var stream = await file.OpenReadAsync();
+                using var memoryStream = new MemoryStream();
+                await stream.CopyToAsync(memoryStream);
+                var imageBytes = memoryStream.ToArray();
+
+                // 解析原文件扩展名
+                string fileName = file.FileName;
+                string ext = Path.GetExtension(fileName);
+
+                // 设置上传用的文件名
+                var uploadFileName = $"{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}{ext}"; //上传后文件名
+                var key = dir + "${filename}";
+
+                // 设置文件的Content
+                var fileContent = new ByteArrayContent(imageBytes);
+                var disposition = new ContentDispositionHeaderValue("form-data")
+                {
+                    Name = "\"file\"",
+                    FileName = $"\"{uploadFileName}\"",
+                };
+
+                fileContent.Headers.ContentDisposition = disposition;
+                string mimeType = ext.ToLowerInvariant() switch
+                {
+                    ".png" => "image/png",
+                    ".jpg" or ".jpeg" => "image/jpeg",
+                    ".gif" => "image/gif",
+                    ".webp" => "image/webp",
+                    ".bmp" => "image/bmp",
+                    _ => "application/octet-stream" // 默认回退
+                };
+
+                fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse(mimeType); //设置媒体类型
+
+                // 添加验证数据头
+                var nameContent = new StringContent(uploadFileName);
+                var keyContent = new StringContent(key);
+                var policyContent = new StringContent(policy);
+                var accessKeyContent = new StringContent(accessKeyId);
+                var successContent = new StringContent("200");
+                var signatureContent = new StringContent(signature);
+
+                // 溢出StringContent自带的ContentType
+                nameContent.Headers.ContentType = null;
+                keyContent.Headers.ContentType = null;
+                policyContent.Headers.ContentType = null;
+                accessKeyContent.Headers.ContentType = null;
+                successContent.Headers.ContentType = null;
+                signatureContent.Headers.ContentType = null;
+
+                // 构造上传数据
+                using var formData = new MultipartFormDataContent();
+                formData.Add(nameContent, "\"name\"");
+                formData.Add(keyContent, "\"key\"");
+                formData.Add(policyContent, "\"policy\"");
+                formData.Add(accessKeyContent, "\"OSSAccessKeyId\"");
+                formData.Add(successContent, "\"success_action_status\"");
+                formData.Add(signatureContent, "\"signature\"");
+                formData.Add(fileContent, "file", $"\"{uploadFileName}\"");
+
+                // 提取自动生成的Boundary值
+                string? contentTypeHeader = formData.Headers.ContentType?.ToString();
+
+                string? boundary = null;
+                if (contentTypeHeader != null)
+                {
+                    var mediaType = MediaTypeHeaderValue.Parse(contentTypeHeader);
+                    boundary = mediaType.Parameters
+                        .FirstOrDefault(p => p.Name.Equals("boundary", StringComparison.OrdinalIgnoreCase))?
+                        .Value!.Trim('"');
+                }
+
+                // 对dotnet自动生成的Content-Type进行替换，去除自动生成的头里面boundary包含的""号，用以兼容阿里云OSS（Fk OSS）
+                formData.Headers.ContentType = new MediaTypeHeaderValue("multipart/form-data")
+                {
+                    Parameters = { new NameValueHeaderValue("boundary", boundary) }
+                };
+
+                // 上传
+                using var httpClient = new HttpClient();
+
+                var result = await httpClient.PostAsync(ossHost, formData);
+
+                if (!result.IsSuccessStatusCode)
+                {
+                    Debug.Fail(await result.Content.ReadAsStringAsync());
+                }
+                return uploadFileName;
+            }
+            catch (Exception ex)
+            {
+                Debug.Fail(ex.Message);
+            }
+
+            return string.Empty;
         }
     }
 }
